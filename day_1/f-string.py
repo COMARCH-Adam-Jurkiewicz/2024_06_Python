9a85aa59f06837a6f44509276b54232bdba9d2a9
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2024-06-24 12:03:35 +0200

    update

---
+++ day_1/f-string.py
@@ -0,0 +1,14 @@
+year = 1974
+name = "Adam"
+
+print(name + "urodził się w roku" + year)
+
+
+print(name + "urodził się w roku" + str(year))
+
+
+print(f"{name} urodził się w roku {year}")
+
+
+print("{name} urodził się w roku {year}")
+
