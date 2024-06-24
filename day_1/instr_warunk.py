6f042963d19e210aa65c4cc3c230f211f58f1e40
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2024-06-24 13:19:17 +0200

    update

---
+++ day_1/instr_warunk.py
@@ -0,0 +1,12 @@
+name = "Adam"
+birth = input(f"{name} - podaj swój rok urodzenia: ")
+age = 2024 - birth
+
+# https://peps.python.org/pep-0008/#code-lay-out
+
+if age >= 18:
+    print("dorosłym być ciekawie")
+    print("OK")
+else:
+    brakuje = 18 - age
+    print(f"Brakuje ci jeszcze {brakuje} lat")
