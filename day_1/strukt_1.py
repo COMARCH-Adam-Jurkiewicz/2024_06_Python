ae57df91bb09a7f9481f891869269d696563b105
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2024-06-24 14:14:43 +0200

    update

--- day_1/strukt_1.py
+++ day_1/strukt_1.py
@@ -1,5 +1,6 @@
 # lista
-names = ["Adam", "Beata", "Szymon"]
+names = [22, "Adam",13, "Beata", "Szymon", 23, True]
 
 for name in names:
     print(f"Imię: {name}")
+    print(f"Imię * 2: {name*2}")
