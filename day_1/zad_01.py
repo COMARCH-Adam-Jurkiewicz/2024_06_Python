90fd24d0878f479a648480748671a4fc208b743c
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2024-06-24 15:16:23 +0200

    update

--- day_1/zad_01.py
+++ day_1/zad_01.py
@@ -1,4 +1,4 @@
-# zdefiniuj putstą listę
+# zdefiniuj pustą listę
 # zapytaj się o wartość końcową
 # w pętli while odliczaj od 1 do podanej wartości
 # co 1000 zadaj pytanie o cokolwiek, i to cokolwiek dodaj do listy
