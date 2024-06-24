ce160f5eac314f2861c1557ac3bd0833adf86848
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2024-06-24 13:55:37 +0200

    update

--- instr_war_cw.py
+++ instr_war_cw.py
@@ -1,3 +1,6 @@
 # jak w poprzednim, zapytaj się o imię
 # jest imię to "Beata", wypisz info: "To imię żony Adama"
 # w innym przypadku wypisz info: "To nie żona Adama"
+
+# Tak jest operator porównania to ==
+# dwa znaki równości
