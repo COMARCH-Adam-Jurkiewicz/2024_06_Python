80c029f4df020ad0e1802755968deb51f27b03f7
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2024-06-25 11:25:44 +0200

    update

--- day_2/csv_read1.py
+++ day_2/csv_read1.py
@@ -1,2 +1,7 @@
 import csv
 plik = "OMNI_PICKUP_CONFIRMATION_DELV2024_06_20.csv"
+
+with open(plik, "r") as csv_file_descriptor:
+    csv_read_object = csv.reader(csv_file_descriptor,  delimiter=';')
+    for data_row in csv_read_object:
+        print(f"{data_row=}")
