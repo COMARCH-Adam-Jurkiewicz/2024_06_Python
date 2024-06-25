a93cfac47dd4ce7eb6233b6a9fd4880893900303
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2024-06-25 11:37:56 +0200

    update

--- day_2/csv_read2.py
+++ day_2/csv_read2.py
@@ -5,7 +5,10 @@ with open(plik, "r") as csv_file_descriptor:
     csv_read_object = csv.reader(csv_file_descriptor,  delimiter=';')
     for data_row in csv_read_object:
         # print(f"{data_row=}")
-        sales_office = data_row[5]
-        if sales_office == "POSAgent":
-            print(f"{data_row=}")
+        try:
+            sales_office = data_row[5]
+            if sales_office == "POSAgent":
+                print(f"{data_row=}")
+        except:
+            pass
 
