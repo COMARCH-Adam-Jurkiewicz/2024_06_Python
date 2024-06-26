2ecb661dd3577b0cdc9ce546298fc361e272119e
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2024-06-26 10:05:51 +0200

    update

---
+++ day_3/skladnia - przykłady/python_3.py
@@ -0,0 +1,15 @@
+import math  # import całego modułu
+# from math import sin, radians  # import tylko 1 funkcji z modułu
+
+
+# spytać usera o ilość stopni
+try:
+    stopnie = float(input("Pdaj nam stopnie do sinusa: "))
+except:
+    print("Źle wprowadzono wartość stopni.")
+
+# obliczyś sin i wyświetlić
+radiany = math.radians(stopnie)
+sinus = math.sin(radiany)
+
+print(f"Sinus z {stopnie} wynosi {sinus}.")
