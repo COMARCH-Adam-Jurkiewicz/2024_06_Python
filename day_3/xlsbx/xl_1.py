b54fc3a05608c490d6ad81e13b56cd68172bbfe5
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2024-06-26 15:27:43 +0200

    update

--- day_3/xlsbx/xl_1.py
+++ day_3/xlsbx/xl_1.py
@@ -1,2 +1,25 @@
-# importy
+# import
 from pyxlsb import open_workbook
+
+# do zmiany na plik inny
+nazwa_xlsb = "test.xlsb"
+
+# otwieramy arkusz - menadżer kontekstowy:
+with open_workbook(nazwa_xlsb) as arkusz_excel:
+    # blok kodu z otwartym do odczytu plikiem
+    print(f"{arkusz_excel.sheets=}")   # "DANE"
+    # otwieramy konkretny arkusz z otwatego pliku xlsb
+    with arkusz_excel.get_sheet("DANE") as sheet:
+        # iterujemy po wszystkich wierszach arkusza...
+        for wiersz in sheet.rows():
+            # print(f"{wiersz=}")
+
+            # generator listowy - tworzy listę wartości wiersza z SHEET
+            values = [element.v for element in wiersz]
+            # generuję statys - kolumna 24 + 25
+            status = f"{values[24]}_{values[25]}"
+            print(f"{status=}")
+
+
+# koniec otwarcia - zamknięty plik
+print("Koniec")
