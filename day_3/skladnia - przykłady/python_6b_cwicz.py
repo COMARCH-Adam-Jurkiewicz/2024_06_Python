48715b72b5d38c9a437879b4e91ee074c31b8871
Adam Jurkiewicz Pythonista <adam@jurkiewicz.tech>
2024-06-26 14:13:01 +0200

    update

--- day_3/skladnia - przykłady/python_6b_cwicz.py
+++ day_3/skladnia - przykłady/python_6b_cwicz.py
@@ -3,18 +3,41 @@
 # funkcje
 
 def il_miejsc_w_samolocie(a, b):
-    # oblicz il. miejsc w samolocie = (a*8 + b*5)
-    # jeśli il. miejsc > 100, to zwróc napis "Airbus", w iinym przypadku
-    # zwróć napis "Boeing"
+    if (a*8 + b*5) > 100:
+        return "Airbus"
+    else:
+        return "Boeing"
+
+def bierz_liczbe(napis):
+    while True:
+        try:
+            liczba = int(input(napis))
+            return liczba
+        except:
+            print("Wprowadź liczbę całkowitą")
 
 def pobieraj_liczby():
-    # pobierze od usera 2 liczby: a,b
-    # jeśli obie będą > 0, to return a,b
+    while True:
+        a = bierz_liczbe("Podaj liczbę a:")
+        if a > 0:
+            break
+        else:
+            print("Podaj liczbę dodatnią.")  # blok pętli
+
+    while True:
+        b = bierz_liczbe("Podaj liczbę b:")
+        if b > 5:
+            break
+        else:
+            print("Podaj liczbę dodatnią > 5.")   # blok pętli
+
+    return a, b # koniec, zwracamy wartość
+
 
 # kod programu
 
 for _ in range(4):
     # pobieraj liczby
-    a,b = pobieraj_liczby()
+    a, b = pobieraj_liczby()
     typ_sam = il_miejsc_w_samolocie(a, b)
-    #  wyświetl napis o typie samolotu : A.., B...
+    print(f"Dla {a=} i {b=} -> {typ_sam=}")
